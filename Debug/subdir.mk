8666e8992fec911dd84e6c3891301299fab609bd
werner <wernerthiago@gmail.com>
2016-02-22 08:48:52 -0300

    Problemas com o UDP

--- Debug/subdir.mk
+++ Debug/subdir.mk
@@ -7,22 +7,28 @@ CPP_SRCS += \
 ../Cliente.cpp \
 ../Evento.cpp \
 ../main.cpp \
+../rcvUDP.cpp \
 ../singleton_client.cpp \
-../singleton_server.cpp 
+../singleton_server.cpp \
+../testUDP.cpp 
 
 OBJS += \
 ./Cliente.o \
 ./Evento.o \
 ./main.o \
+./rcvUDP.o \
 ./singleton_client.o \
-./singleton_server.o 
+./singleton_server.o \
+./testUDP.o 
 
 CPP_DEPS += \
 ./Cliente.d \
 ./Evento.d \
 ./main.d \
+./rcvUDP.d \
 ./singleton_client.d \
-./singleton_server.d 
+./singleton_server.d \
+./testUDP.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
